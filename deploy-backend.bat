b20e32a4dad2e093e51bfd1c410d8c9063e798a8
RTAkland <rtakland@outlook.com>
2025-04-14 03:34:43 +0800

    Move deploy script

--- deploy-backend.bat
+++ deploy-backend.bat
@@ -1,22 +1,23 @@
 @echo off
 set SERVER=root@lan.rtast.cn
 set REMOTE_DIR=/root/reposilite
-set LOCAL_FILE=build\bin\linuxX64\releaseExecutable\backend.kexe
+set LOCAL_FILE=backend\build\bin\linuxX64\releaseExecutable\backend.kexe
 
-echo [1/4] Checking artifacts...
+
+echo Checking artifacts...
 if not exist %LOCAL_FILE% (
     echo Artifact not found: %LOCAL_FILE%
     exit /b 1
 )
 
-echo [2/4] Uploading...
+echo Uploading...
 scp %LOCAL_FILE% %SERVER%:/tmp/backend.kexe
 if errorlevel 1 (
     echo Upload failure
     exit /b 1
 )
 
-echo [3/4] Moving file and restart services...
+echo Moving file and restart services...
 ssh %SERVER% "systemctl stop reposilite && rm %REMOTE_DIR%/backend.kexe || true && mv /tmp/backend.kexe %REMOTE_DIR%/backend.kexe && chmod +x %REMOTE_DIR%/backend.kexe && systemctl restart reposilite"
 
 if errorlevel 1 (
@@ -24,4 +25,6 @@ if errorlevel 1 (
     exit /b 1
 )
 
-echo [4/4] Deployment success！
+echo Deployment success！
+
+pause
