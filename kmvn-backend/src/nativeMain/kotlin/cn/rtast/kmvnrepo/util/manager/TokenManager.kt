fb1ff9d6c9c303250f7ebee8379f17f26b86a4bc
RTAkland <rtakland@outlook.com>
2025-05-09 04:00:07 +0800

    Fix backend auth cache

--- kmvn-backend/src/nativeMain/kotlin/cn/rtast/kmvnrepo/util/manager/TokenManager.kt
+++ kmvn-backend/src/nativeMain/kotlin/cn/rtast/kmvnrepo/util/manager/TokenManager.kt
@@ -5,30 +5,39 @@
  * https://www.apache.org/licenses/LICENSE-2.0
  */
 
-@file:OptIn(ExperimentalUuidApi::class)
+@file:OptIn(ExperimentalUuidApi::class, ExperimentalTime::class)
 
 package cn.rtast.kmvnrepo.util.manager
 
+import cn.rtast.kmvnrepo.entity.auth.TokenPayload
 import cn.rtast.kmvnrepo.util.generateSecureRandomString
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 import kotlin.uuid.ExperimentalUuidApi
 
 class TokenManager {
-    private val validatedTokens = mutableMapOf<String, String>()
+    private val validatedTokens = mutableListOf<TokenPayload>()
 
-    fun revoke(name: String): String? {
-        return validatedTokens.remove(name)
+    fun revoke(name: String) {
+        validatedTokens.removeAll { it.name == name }
     }
 
     fun grant(name: String): String {
         return generateSecureRandomString().apply {
-            if (validatedTokens.containsKey(name)) {
-                validatedTokens.remove(name)
+            if (validatedTokens.any { it.name == name }) {
+                this@TokenManager.revoke(name)
             }
-            validatedTokens[name] = this
+            validatedTokens.add(TokenPayload(name, this, Clock.System.now().epochSeconds + 7 * 24 * 60 * 60))
         }
     }
 
-    fun getName(id: String): String? = validatedTokens.entries.find { it.value == id }?.key
+    fun getName(id: String): String? = validatedTokens.find { it.value == id }?.name
 
-    fun validate(id: String): Boolean = validatedTokens.containsValue(id)
+    fun validate(id: String): Boolean {
+        val payload = validatedTokens.find { it.value == id } ?: return false
+        if (payload.expiredAt <= Clock.System.now().epochSeconds) {
+            this.revoke(payload.name)
+            return false
+        } else return true
+    }
 }
